d58c4e052a3df294f59481c2bdcfa28c903f7475
harsc <harsc@HarshithGowda77>
2026-07-21 10:40:00 +0530

    first commit

---
+++ TNSIF_java/src/org/insif/acc/c2tc/dataype_typecasting/TypeCastingDemo.java
@@ -0,0 +1,25 @@
+package org.insif.acc.c2tc.dataype_typecasting;
+
+public class TypeCastingDemo {
+
+    public static void main(String[] args) {
+        // Shopping Cart
+
+        // Widening (small → large)
+        int quantity = 3;
+        double pricePerItem = 99.50;
+        double totalPrice = quantity * pricePerItem;
+
+        // Narrowing (large → small)
+        double discount = 10.75;
+        int roundedDiscount = (int) discount; // truncates decimal part
+
+        double finalAmount = totalPrice - roundedDiscount;
+
+        // Output
+        System.out.println("Online Shopping Summary");
+        System.out.println("Items Bought: " + quantity);
+        System.out.println("Price per item: " + pricePerItem);
+        System.out.println("Final Amount to pay after discount: " + finalAmount);
+    }
+}
