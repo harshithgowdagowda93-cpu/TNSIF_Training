a2a21bee1b0e04f05b6af9b5c790eabc3a0c948e
harsc <harsc@HarshithGowda77>
2026-07-23 11:34:57 +0530

    first commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/manager/EncapsulationDemo.java
@@ -0,0 +1,43 @@
+package org.tnsif.acc.c2tc.manager;
+
+class Human{
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		Human obj=new Human();
+		obj.setAge(26);
+        obj.setName("Ganesh");
+
+        System.out.println(obj.getAge());
+        System.out.println(obj.getName());
+
+        obj.setAge(22);
+        obj.setName("Harshith");
+
+        System.out.println(obj.getAge());
+        System.out.println(obj.getName());
+	    
+	    
+	    
+
+	}
+
+}
