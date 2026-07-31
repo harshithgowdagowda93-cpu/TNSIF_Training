d72337d4e602cc67dd4d6f2c748d045bab4e9e30
harsc <harsc@HarshithGowda77>
2026-07-31 10:02:33 +0530

    first commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/oops/PolymorphismDemo1.java
@@ -0,0 +1,28 @@
+package org.tnsif.acc.c2tc.oops;
+
+class addition
+{
+int add(int num1,int num2)
+{
+return num1+num2;
+}
+double add(double num1,double num2)
+{
+return num1+num2;
+}
+int add(int num1,int num2,int num3)
+{
+return num1+num2+num3;
+}
+}
+public class PolymorphismDemo1 {
+
+	public static void main(String[] args) {
+		addition add=new addition();
+		System.out.println("Two integers sum "+add.add(1, 3));
+		System.out.println("Three integres sum "+add.add(3, 6,8));
+		System.out.println("Two double datatype sum "+add.add(3.5,5.6));
+		
+	}
+
+}
