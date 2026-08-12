f8890b05e662845f64b1f6c68642b952605cfbf0
harsc <harsc@HarshithGowda77>
2026-08-12 10:53:18 +0530

    first commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/tnsif/interfacedemo/Default_Static_Demo1.java
@@ -0,0 +1,76 @@
+package org.tnsif.acc.c2tc.tnsif.interfacedemo;
+
+interface Vehicle {
+
+    void start();
+    void stop();
+
+    default void repair() {
+        System.out.println("General vehicle repair service");
+    }
+}
+
+class Car implements Vehicle {
+
+    private String model;
+
+    Car(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Car " + model + " starting");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car " + model + " stopping");
+    }
+
+    @Override
+    public void repair() {
+        System.out.println("Car " + model + " specific service");
+    }
+}
+
+class Bike implements Vehicle {
+
+    private String type;
+
+    Bike(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Bike " + type + " starting");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Bike " + type + " stopping");
+    }
+
+    @Override
+    public void repair() {
+        System.out.println("Bike " + type + " specific service");
+    }
+}
+
+public class Default_Static_Demo1 {
+
+    public static void main(String[] args) {
+
+        Vehicle car = new Car("KIA");
+        Vehicle bike = new Bike("RX 100");
+
+        car.start();
+        car.stop();
+        car.repair();
+
+        bike.start();
+        bike.stop();
+        bike.repair();
+    }
+}
