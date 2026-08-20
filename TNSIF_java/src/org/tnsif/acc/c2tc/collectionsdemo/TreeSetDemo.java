24d6d82d7e3067d2c979cfa7807ea4bad3a3e66b
harsc <harsc@HarshithGowda77>
2026-08-20 10:39:17 +0530

    first commit

---
+++ TNSIF_java/src/org/tnsif/acc/c2tc/collectionsdemo/TreeSetDemo.java
@@ -0,0 +1,16 @@
+package org.tnsif.acc.c2tc.collectionsdemo;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		TreeSet<String> set=new TreeSet<>();
+		set.add("Cherry");
+		set.add("Banana");
+		set.add("Apple");
+		System.out.println("TreeSet "+set);
+
+	}
+
+}
